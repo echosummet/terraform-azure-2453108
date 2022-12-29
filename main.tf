dfbadf72065b082432242d447e6a6023724f27e1
uoɔɐq <74855801+echosummet@users.noreply.github.com>
2022-12-28 16:00:38 -0800

    d

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "main" {
 
 #Creates virtual network
 resource "azurerm_virtual_network" "main" {
-  name                = "learn-tf-vnet-eastus"
+  name                = "learn-tf-vnet-westus2"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
